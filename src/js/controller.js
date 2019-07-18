caf74cb4b1a4997ecbd013139a672addb448873a
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-07-18 16:40:47 -0700

    Lol really skewed cube

--- src/js/controller.js
+++ src/js/controller.js
@@ -37,13 +37,13 @@ export default class Controller {
 	updateProjections() {
 		this.dimensionProjections = [];
 		for (let i = 0; i < this.currentIntDimension; i++) {
-			let amt = 0;
-			if (i == this.currentIntDimension - 1) {
-				amt = i / this.currentIntDimension;
-			}
-			else {
-				amt = i / (this.currentDimension - 1);
-			}
+			let amt = i / (i + 1); 
+			// if (i == this.currentIntDimension - 1) {
+			// 	amt = i / this.currentIntDimension;
+			// }
+			// else {
+			// 	amt = i / (this.currentDimension - 1);
+			// }
 			const angle = Math.PI * amt;
 			const projection = {
 				x: Math.cos(angle),
